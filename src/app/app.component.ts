8f17efbab7d247d42187b3a398c852523258a629
PogiAbel <pogonyiabel@gmail.com>
2024-05-23 12:01:09 +0200

    running, basic, starting point

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+async function api<T>(url: string): Promise<T> {
+  return fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.statusText)
+      }
+      return response.json() as Promise<T>
+    })
+}
+let first: Comment;
+
+class Comment {
+  constructor(public id:string, public name: string, public email:string, public movie_id:string,public text:string, public date:Date) { }
+  static fromJson(json: any): Comment {
+    return new Comment(json._id, json.name, json.email, json.movie_id, json.text, json.date)
+  }
+  static fromEmpty(): Comment {
+    return new Comment('', '', '', '', '', new Date())
+  }
+  stringify() {
+    return JSON.stringify(this)
+  }
+}
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +34,11 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'beadando';
+  public com: Comment = Comment.fromEmpty();
+
+  async ngOnInit() {
+    let data: any = await api('http://localhost:3000/comments') as JSON;
+    this.com = Comment.fromJson(data[0]);
+    
+  }
 }
