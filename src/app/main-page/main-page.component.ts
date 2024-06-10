ab4234297e2778cfdd9ee9a53d3101564fca18c6
PogiAbel <pogonyiabel@gmail.com>
2024-06-10 13:28:41 +0200

    added routing, multi component works

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from '../home/home.component';
+import { MovieComponent } from '../movie/movie.component';
 
 async function api<T>(url: string): Promise<T> {
   return fetch(url)
@@ -15,8 +17,30 @@ async function api<T>(url: string): Promise<T> {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
-  templateUrl: './main-page.component.html',
+  imports: [RouterModule, HomeComponent, MovieComponent],
+  template: `
+    <header>
+      <div class="container">
+          <h1>Movie Database</h1>
+          <nav>
+              <ul>
+                  <li><a href="#">Home</a></li>
+                  <li><a href="#">Movies</a></li>
+                  <li><a href="#">Genres</a></li>
+                  <li><a href="#">Contact</a></li>
+              </ul>
+          </nav>
+      </div>
+  </header>
+  <section class="content">
+    <router-outlet></router-outlet>
+  </section>
+  <footer>
+      <div class="container">
+          <p>&copy; 2024 Movie Database. All rights reserved.</p>
+      </div>
+  </footer>
+  `,
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { MainPageComponent } from './app/main-page/main-page.component';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(MainPageComponent)
+
+bootstrapApplication(MainPageComponent,{
+  providers: [provideRouter(routes)]
+
+})
   .catch((err) => console.error(err));
