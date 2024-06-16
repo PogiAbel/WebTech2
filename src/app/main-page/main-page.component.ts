d455dfcf17cd59cbd7a79d024add5994cdbdb5a2
PogiAbel <pogonyiabel@gmail.com>
2024-06-16 16:05:29 +0200

    some css rework, flexbox in upper managment

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -2,37 +2,18 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HomeComponent } from '../home/home.component';
 import { MovieComponent } from '../movie/movie.component';
+import { Login } from '../interfaces';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule, HomeComponent, MovieComponent],
-  template: `
-    <header>
-      <div class="container">
-          <h1>Movie Database</h1>
-          <nav>
-              <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="/movies">Movies</a></li>
-                  <li><a href="#">Genres</a></li>
-                  <li><a href="#">Contact</a></li>
-              </ul>
-          </nav>
-      </div>
-  </header>
-  <section class="content">
-    <router-outlet></router-outlet>
-  </section>
-  <footer>
-      <div class="container">
-          <p>&copy; 2024 Movie Database. All rights reserved.</p>
-      </div>
-  </footer>
-  `,
+  templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
 export class MainPageComponent {
   title = 'beadando';
+
+  private login!: Login;
 }
